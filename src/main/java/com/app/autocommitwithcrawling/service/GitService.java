f3145c5d86fe6f5bbb858755d1c6d8c2e8777d85
gimdaeyeon <rlaeodus1306@naver.com>
2024-04-08 17:46:05 +0900

    Feat: GitService생성시 해당 프로젝트가 git등록되어있지 않으면 init, remote설정 하도록 코드 추가

--- src/main/java/com/app/autocommitwithcrawling/service/GitService.java
+++ src/main/java/com/app/autocommitwithcrawling/service/GitService.java
@@ -1,30 +1,41 @@
 package com.app.autocommitwithcrawling.service;
 
 import com.app.autocommitwithcrawling.domain.entity.CodingSolution;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.RemoteAddCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.transport.RefSpec;
+import org.eclipse.jgit.transport.URIish;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class GitService {
-    private final File GIT_DIR = new File(System.getProperty("user.dir"));
+    private final File GIT_DIR;
     private final UsernamePasswordCredentialsProvider credentialsProvider;
 
 //    todo 해당 프로젝트가 실행될 때 git 프로젝트로 등록하기
 
-    //    public GitService(UsernamePasswordCredentialsProvider credentialsProvider) {
-//        this.credentialsProvider = credentialsProvider;
-//        this.gitDir  = new File(System.getProperty("user.dir"));
-//    }
+    public GitService(UsernamePasswordCredentialsProvider credentialsProvider,
+                      @Value("${git.repoUri}") String gitRepoUri) throws GitAPIException, URISyntaxException {
+
+        this.credentialsProvider = credentialsProvider;
+        this.GIT_DIR = new File(System.getProperty("user.dir"));
+        if (!isGitRepoDir()) {
+            Git git = Git.init().setDirectory(GIT_DIR).call();
+            RemoteAddCommand remoteAddCommand = git.remoteAdd();
+            remoteAddCommand.setName("origin");
+            remoteAddCommand.setUri(new URIish(gitRepoUri));
+            remoteAddCommand.call();
+        }
+    }
 
 
     public void gitCommitAndPush(CodingSolution solution) {
@@ -33,8 +44,8 @@ public class GitService {
             git.add().addFilepattern(".").call();
             git.commit().setMessage(createGitCommitMessage(solution)).call();
             git.pull().setRemoteBranchName("main")
-                            .setCredentialsProvider(credentialsProvider)
-                                    .call();
+                    .setCredentialsProvider(credentialsProvider)
+                    .call();
             git.push()
                     .setCredentialsProvider(credentialsProvider)
                     .setRemote("origin")
@@ -57,6 +68,17 @@ public class GitService {
                '(' +
                solution.getProblemNumber() +
                "번) 등록";
+    }
+
+    private boolean isGitRepoDir() {
+        try {
+            File currentDir = new File(".").getCanonicalFile(); // 현재 디렉토리의 정규 경로를 가져옵니다.
+            File gitDir = new File(currentDir, ".git"); // 현재 디렉토리 내의 .git 폴더를 찾습니다.
+            return gitDir.exists() && gitDir.isDirectory();
+        } catch (IOException e) {
+            log.error("{}", e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
 
